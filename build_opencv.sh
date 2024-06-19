8b94d4fd8770bb6a14c55704f3225148d507cab7
DESKTOP-TPBT7PN\vova <kuksynok.v@gmail.com>
2024-06-19 10:21:30 +0300

    Disabling apt upgrade as it's going to brake bootloader on some custom carrier boards update readme.md

--- build_opencv.sh
+++ build_opencv.sh
@@ -54,7 +54,7 @@ install_dependencies () {
     # package repository or should already be installed (eg. CUDA).
     echo "Installing build dependencies."
     sudo apt-get update
-    sudo apt-get dist-upgrade -y --autoremove
+    #sudo apt-get dist-upgrade -y --autoremove
     sudo apt-get install -y \
         build-essential \
         cmake \
