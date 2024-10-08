bcbbee8b499894fbfd3758fe1d657b31edd2f027
vdoom <vdoom.heretic@gmail.com>
2024-10-08 18:03:48 +0300

    add script for big xavier and update cv_ver check script for getting essential python3 patths for ocv build\install script

--- cv_ver.py
+++ cv_ver.py
@@ -1,3 +1,7 @@
 import cv2
+from distutils.sysconfig import get_python_inc
+from distutils.sysconfig import get_python_lib
 
 print("OpenCV version:", cv2.__version__)
+print(get_python_inc())
+print(get_python_lib())
