f81f258fb37ebf6715325234bda9c81fc1d26610
vdoom <vdoom.heretic@gmail.com>
2025-07-12 08:52:16 +0300

    Show location of cv2 in cv_ver.py script

--- cv_ver.py
+++ cv_ver.py
@@ -3,5 +3,6 @@ from distutils.sysconfig import get_python_inc
 from distutils.sysconfig import get_python_lib
 
 print("OpenCV version:", cv2.__version__)
+print("OpenCV Location: ", cv2.__file__)
 print(get_python_inc())
 print(get_python_lib())
